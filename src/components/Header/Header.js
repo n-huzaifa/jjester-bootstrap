1b56080f77ee60f9f17c8d000b413b9643a5bdf2
noor huzaifa <noorhuzaifa11@gmail.com>
2022-06-01 16:56:37 +0500

    File structuring

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+            <Button variant="dark" className='ButtonText'>JJESTER</Button>
+        </div>
+    )
+}
+
+export default Header
